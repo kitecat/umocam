7218836a827df27c9984ff788d13a7ba982bac9f
aital <aital.egorov@yaknet.com>
2025-03-04 21:37:31 +0900

    Updated libs and configs to build on actual version of Android Studio

---
+++ app/src/main/java/ru/vava/umocam/MainActivity.java
@@ -0,0 +1,47 @@
+package ru.vava.umocam;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        setContentView(R.layout.activity_main);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Включение автоповорота
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+
+        ImageView startDrawButton = (ImageView) findViewById(R.id.startDrawButton);
+        startDrawButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, DrawActivity.class));
+            }
+        });
+
+        ImageView showTutorialButton = (ImageView) findViewById(R.id.showTutorialButton);
+        showTutorialButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, TutorialActivity.class));
+            }
+        });
+
+        ImageView startCalibrateButton = (ImageView) findViewById(R.id.startCalibrateButton);
+        startCalibrateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CalibrateActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/ru/vava/umocam/TutorialActivity.java
@@ -0,0 +1,13 @@
+package ru.vava.umocam;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TutorialActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial);
+    }
+}
